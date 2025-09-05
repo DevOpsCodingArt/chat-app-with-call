7df297ec28908f95a68454d20e7df4b05a774269
Moiz Ahmad <moiz@scholar.tu.edu.pk>
2025-09-05 20:57:32 +0500

    Update BASE_URL for production environment in axios

--- frontend/chat-app/src/libs/axios.js
+++ frontend/chat-app/src/libs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chat-app-with-call.vercel.app/";
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
